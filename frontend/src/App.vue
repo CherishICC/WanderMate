f8ff74a3baf96946e9fb08211cb895b5e5179526
Cherish Inampudi <80279574+CherishICC@users.noreply.github.com>
2022-11-16 13:01:28 +0530

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <router-link to="/" class="navbar-brand">WanderMate</router-link>
+      <router-link to="/" class="navbar-brand">WanderMates</router-link>
       <div class="navbar-nav mr-auto">
         <li class="nav-item">
           <router-link to="/tutorials" class="nav-link">Packages</router-link>
